4f1d0cade40a9ecf43642438ed84804fec4d5a78
dimuthx353 <dimuthx353@gmail.com>
2025-08-27 00:49:37 +0530

    style: Enhance layout and styling for reports index and monthly views

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -3,7 +3,9 @@
         <h2 class="text-2xl font-bold text-white">Reports</h2>
     </x-slot>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-6 mt-32 ">
+        <a href=""></a>
+
         <a href="{{ route('reports.monthly') }}" class="p-6 bg-white rounded-lg shadow hover:bg-gray-50 text-center font-semibold">
             Monthly Payroll Summary
         </a>
@@ -11,5 +13,6 @@
         <a href="{{ route('employees.index') }}" class="p-6 bg-white rounded-lg shadow hover:bg-gray-50 text-center font-semibold">
             Employee Payroll History
         </a>
+        <a href=""></a>
     </div>
 </x-app-layout>
